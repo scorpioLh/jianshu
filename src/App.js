0455a9013eaf6ad636ea8f55b0d5ee8bb15155e2
linhan <scorpio360@163.com>
2019-02-02 22:51:47 +0800

    添加header部分

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { GlobalStyle } from "./style";
+import Header from './common/header'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
 				<GlobalStyle/>
-        hello world
-      </div>
+				<Header />
+      </Fragment>
     );
   }
 }
