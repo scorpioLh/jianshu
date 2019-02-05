5e5b39629d9ff0c5d44d0c11ad0a1a0e3678fd07
linhan <scorpio360@163.com>
2019-02-05 22:06:59 +0800

    整理代码

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -6,11 +6,11 @@ const defaultState = fromJS({
 })
 
 export default (state = defaultState, action) => {
-    if (action.type === constants.SEARCH_FOCUS) {
-        return state.set('focused', true)
-    }
-    if (action.type === constants.SEARCH_BLUR) {
-        return state.set('focused', false)
-    }
-    return state
+  if (action.type === constants.SEARCH_FOCUS) {
+    return state.set('focused', true)
+  }
+  if (action.type === constants.SEARCH_BLUR) {
+    return state.set('focused', false)
+  }
+  return state
 }
